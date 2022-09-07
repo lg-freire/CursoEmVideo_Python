c4c1c6b39b4086b393209c1f6cf78fd3b34f28e9
lg-freire <l-gabriel@outlook.com>
2022-09-06 23:29:54 -0300

    Ex 62 corrigido

--- Exercícios/Ex 062.py
+++ Exercícios/Ex 062.py
@@ -1,11 +1,13 @@
 n = int(input('Input a number: '))
 r = int(input('Input the factor: '))
-count = 11
+count, tcount = 11, 0
 while count != 0:
     if count > 1:
-        print(f'{n} ', end='')
+        print(f'{n}', end='')
+        print(' → ' if count > 2 else ' → PAUSE', end='')
         n += r
         count -= 1
+        tcount += 1
     else:
         add = int(input('''\nWould you like to continue with the progression?
 Input how many more instances you'd like to see, or input 0 to exit: '''))
@@ -13,3 +15,4 @@ Input how many more instances you'd like to see, or input 0 to exit: '''))
             count += add
         else:
             count = 0
+print(f'Process finished with {tcount} readings.')
