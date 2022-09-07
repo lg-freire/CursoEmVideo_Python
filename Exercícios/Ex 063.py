39211df8f4bfc836c31d3e4cfeec486d9a409bf0
lg-freire <l-gabriel@outlook.com>
2022-09-06 23:35:37 -0300

    Ex 63 corrigido

--- Exercícios/Ex 063.py
+++ Exercícios/Ex 063.py
@@ -1,11 +1,10 @@
 n = int(input("Input the number of digits of the Fibonacci sequence you'd like to see: "))
+print('~' * 15, 'FIBONACCI', '~' * 15)
 print('0', end=' > ')
 f, fm, fm2 = 1, 1, 0
 while n != 0:
-    if n > 1:
-        print(f, end=' > ')
-    else:
-        print(f, end='')
+    print(f, end='')
+    print(' → ' if n > 1 else ' → END', end='')
     f = fm + fm2
     fm2 = fm
     fm = f
