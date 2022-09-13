2d3796c687706957723e27db5c7b2b76c5ca9afa
lg-freire <l-gabriel@outlook.com>
2022-09-13 16:35:55 -0300

    Ex 76 test

--- Exercícios/Ex 076.py
+++ Exercícios/Ex 076.py
@@ -1,9 +1,9 @@
-li = ('Dildo', 6.66, 'Jabroni Outfit', 82.99, 'Gym Membership', 39.99)
+li = ('Dildo', 6.66, 'Jabroni Outfit', 82.99, 'Gym Membership', 39.99, 'Hose', 3.5)
 print('-' * 39)
 print(f'{"PRICE LISTING":^39}')
 print('-' * 39)
 
 for i in range(0, len(li), 2):
-    print(f"{li[i]:.<30}US${li[i+1]:6}")
+    print(f"{li[i]:.<30}US${li[i+1]:>6.2f}")
 
 print('-' * 39)
