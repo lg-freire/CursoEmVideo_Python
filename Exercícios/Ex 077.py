83d597c95560ac0a257232fc1f528456c5a79036
lg-freire <l-gabriel@outlook.com>
2022-09-12 22:01:44 -0300

    Ex 77

--- Exercícios/Ex 077.py
+++ Exercícios/Ex 077.py
@@ -1,6 +1,6 @@
 li = ('Thots', 'Carnivore', 'Jabroni', 'Pauper', 'Semblance', 'Arrakis', 'Dune', 'Chapterhouse', 'Simple')
 for word in li:
-    print(f'The vowels of the word {word} are ', end='')
+    print(f'\033[31m{word}\033[m: ', end='')
     for i in range(0, len(word)):
         if word[i] in 'AaEeIiOoUu':
             print(word[i], end=' ')
