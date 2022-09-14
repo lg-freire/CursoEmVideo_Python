d5a9103dad9149522867dbf7f998a4dc9f2dc790
lg-freire <l-gabriel@outlook.com>
2022-09-14 19:12:36 -0300

    Ex 87 corrigido

--- Exercícios/Ex 087.py
+++ Exercícios/Ex 087.py
@@ -1,26 +1,22 @@
-nums = []
+nums = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 s = cs = m = 0
 
 for i in range(0, 3):
     for j in range(0, 3):
-        nums.append(int(input(f'Value for [{i}, {j}]: ')))
+        nums[i][j] = (int(input(f'Value for [{i}, {j}]: ')))
 
-for f, i in enumerate(nums):
-    print(f'[{i:^7}]', end='')
-    if (f + 1) % 3 == 0:
-        print()
+for i in range(0, 3):
+    for j in range(0, 3):
+        print(f'[{nums[i][j]:^6}]', end='')
+        if nums[i][j] % 2 == 0:
+            s += nums[i][j]
+    print()
 
-for n in nums:
-    if n % 2 == 0:
-        s += n
 print(f'The sum of all even numbers is {s}.')
 
-for n in nums:
-    if n % 3 == 0:
-        cs += n
+for n in range(0, 3):
+    cs += nums[n][2]
 print(f'The sum of all numbers in the third row is {cs}.')
 
-for i in range(3, 6):
-    if nums[i] > m:
-        m = nums[i]
+m = max(nums[1])
 print(f'The highest number in the second line is {m}.')
