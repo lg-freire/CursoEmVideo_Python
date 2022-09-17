f322d1fa8e4a815520475b61bcf44c9cd27b2e74
lg-freire <l-gabriel@outlook.com>
2022-09-17 13:06:33 -0300

    Ex 105 corrigido

--- Exercícios/Ex 105.py
+++ Exercícios/Ex 105.py
@@ -3,7 +3,7 @@ def scores(*n, sit=False):
     → Function for analyzing scores and averages of a student.
     :param n: One or more of a student's scores.
     :param sit: (optional) Show if the student has generally GOOD, AVERAGE or BAD scores.
-    :return: A dictionary with various informations pertaining the presented scores.
+    :return: A dictionary with various information pertaining the presented scores.
     """
     avg = sum(n) / len(n)
     dc = {'total': len(n), 'highest': (max(n)), 'lowest': min(n), 'average': f'{avg:.1f}'}
