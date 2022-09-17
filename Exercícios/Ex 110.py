abece8da31a35262f2b299f223dac478810a7f59
lg-freire <l-gabriel@outlook.com>
2022-09-17 15:07:36 -0300

    Ex 110

---
+++ Exercícios/Ex 110.py
@@ -0,0 +1,4 @@
+from Modules import money
+
+n = float(input('Price: US$'))
+money.table(n)
--- Exercícios/Modules/money/__init__.py
+++ Exercícios/Modules/money/__init__.py
@@ -1,3 +1,6 @@
+from MyFunctions import title
+
+
 def half(n, flag=False):
     s = n / 2
     if flag:
@@ -32,3 +35,12 @@ def discount(n, d=0, flag=False):
 
 def money(n):
     return f'US${n:.2f}'
+
+
+def table(n):
+    title('VALUE BREAKDOWN', 40)
+    print(f"""{'Analyzed value:':30}{money(n):10}
+{'Double price:':30}{double(n, True):10}
+{'Half price:':30}{half(n, True):10}
+{'40 % increase:':30}{increase(n, 40, True):10}
+{'30% discount:':30}{discount(n, 30, True)}""")
