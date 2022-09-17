4030a17d3044c356c1f2fbaeb79d31951ab587ae
lg-freire <l-gabriel@outlook.com>
2022-09-17 20:26:53 -0300

    Ex 113

---
+++ Exercícios/Ex 113.py
@@ -0,0 +1,35 @@
+def validateInt():
+    while True:
+        try:
+            n = int(input('Type an integer number: '))
+            break
+        except ValueError as error:
+            if error.__cause__ is None:
+                print('\033[31mUser chose not to input this value\033[m')
+                n = 0
+                break
+            else:
+                print('''\033[1;31mINVALID INPUT\033[m
+Please input a valid number.''')
+    return n
+
+
+def validateFloat():
+    while True:
+        try:
+            n = float(input('Type a floating point number: '))
+            break
+        except ValueError as error:
+            if error.__cause__ is None:
+                print('\033[31mUser chose not to input this value\033[m')
+                n = 0
+                break
+            else:
+                print('''\033[1;31mINVALID INPUT\033[m
+        Please input a valid number.''')
+    return n
+
+
+n1 = validateInt()
+n2 = validateFloat()
+print(f'You typed the integer {n1} and the float value {n2}.')
